d237333a605700d00e865eaf8f4425a9c5fe061c
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2020-11-03 18:05:57 +0900

    クイズ画面おおよそ完成

--- app/controllers/level_controller.rb
+++ app/controllers/level_controller.rb
@@ -2,6 +2,7 @@ class LevelController < ApplicationController
   def question
     @level = Level.find_by(id: params[:id])
     @questions = @level.questions
+    @choices = @questions.map{ |question| question.choices }
   end
 
   def ranking
