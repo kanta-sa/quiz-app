cd39fb0b06d9e62e506dfa6f4cfa7965e2279207
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2020-11-25 19:25:38 +0900

    クイズのデータセット

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
 
-# レベルのモデル
-level = (1..3).map do |i|
-  l = {
-    name: "レベル#{i}"
-  }
-  l
+# 使用するデータ(CSVファイルの列)を指定
+CSVROW_LEVELID = 1
+CSVROW_LEVELNAME = 2
+CSVROW_QUESTIONID = 3
+CSVROW_QUESTIONCONTENT = 4
+CSVROW_CHOICECONTENT = 5
+CSVROW_CHOICEBOOL = 6
+
+CSV.foreach('db/csv/question.csv') do |row|
+  level_id = row[CSVROW_LEVELID]
+  level_name = row[CSVROW_LEVELNAME]
+  question_id = row[CSVROW_QUESTIONID]
+  question_content = row[CSVROW_QUESTIONCONTENT]
+  choice_content = row[CSVROW_CHOICECONTENT]
+  choice_bool = row[CSVROW_CHOICEBOOL]
+
+  Level.find_or_create_by(name: level_name)
+  Question.find_or_create_by(content: question_content, level_id: level_id)
+  Choice.create!(content: choice_content, is_answer: choice_bool, question_id: question_id)
 end
-Level.create!(level)
+
+User.create!(username: 'ユーザー1',
+             email: 'user1@example.com',
+             password: 'password',
+             password_confirmation: 'password'
+)
