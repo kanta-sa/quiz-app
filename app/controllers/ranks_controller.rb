3c2ffa0134e7f48cd46dd1aedd3b7b7fc4fcb813
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2020-12-10 20:00:26 +0900

    言語選択によって遷移先を場合分け

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -2,18 +2,34 @@ class RanksController < ApplicationController
   def create
     @rank = Rank.new(level_id: params[:rank][:level_id], user_id: params[:rank][:user_id], score: params[:rank][:score])
     if @rank.save
-      redirect_to root_url
+      if params[:rank][:type] == 'ja'
+        redirect_to root_url
+      else
+        redirect_to en_top_url
+      end
     else
-      redirect_to tests_url
+      if params[:rank][:type] == 'ja'
+        redirect_to tests_url
+      else
+        redirect_to en_tests_url
+      end
     end
   end
 
   def update
     @rank = Rank.where(level_id: params[:rank][:level_id], user_id: params[:rank][:user_id])
     if @rank.update_all(score: params[:rank][:score])
-      redirect_to root_url
+      if params[:rank][:type] == 'ja'
+        redirect_to root_url
+      else
+        redirect_to en_top_url
+      end
     else
-      redirect_to tests_url
+      if params[:rank][:type] == 'ja'
+        redirect_to tests_url
+      else
+        redirect_to en_tests_url
+      end
     end
   end
 end
