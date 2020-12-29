1f5ff7dd175172648ab016cfd5bd32e2617a4da7
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2020-12-29 10:35:15 +0900

    仮デプロイ

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,51 +15,51 @@ CSVROW_SUBJECT = 2
 CSVROW_MEANING_JA = 3
 CSVROW_MEANING_EN = 4
 
-CSV.foreach('db/csv/question.csv') do |row|
-  level_id = row[CSVROW_LEVELID]
-  level_name = row[CSVROW_LEVELNAME]
-  question_id = row[CSVROW_QUESTIONID]
-  question_content_ja = row[CSVROW_QUESTIONCONTENT_JA]
-  question_content_en = row[CSVROW_QUESTIONCONTENT_EN]
-  choice_content = row[CSVROW_CHOICECONTENT]
-  choice_bool = row[CSVROW_CHOICEBOOL]
+# CSV.foreach('db/csv/question.csv') do |row|
+#   level_id = row[CSVROW_LEVELID]
+#   level_name = row[CSVROW_LEVELNAME]
+#   question_id = row[CSVROW_QUESTIONID]
+#   question_content_ja = row[CSVROW_QUESTIONCONTENT_JA]
+#   question_content_en = row[CSVROW_QUESTIONCONTENT_EN]
+#   choice_content = row[CSVROW_CHOICECONTENT]
+#   choice_bool = row[CSVROW_CHOICEBOOL]
+#
+#   Level.find_or_create_by(name: level_name)
+#   q = Question.find_by(
+#     content_ja: question_content_ja,
+#     content_en: question_content_en,
+#     level_id: level_id
+#   )
+#   if q.nil?
+#     Question.create!(
+#       content_ja: question_content_ja,
+#       content_en: question_content_en,
+#       level_id: level_id,
+#       image: open("./db/fixtures/rails.png")
+#     )
+#   end
+#   Choice.create!(
+#     content: choice_content,
+#     is_answer: choice_bool,
+#     question_id: question_id
+#   )
+# end
 
-  Level.find_or_create_by(name: level_name)
-  q = Question.find_by(
-    content_ja: question_content_ja,
-    content_en: question_content_en,
-    level_id: level_id
-  )
-  if q.nil?
-    Question.create!(
-      content_ja: question_content_ja,
-      content_en: question_content_en,
-      level_id: level_id,
-      image: open("./db/fixtures/rails.png")
-    )
-  end
-  Choice.create!(
-    content: choice_content,
-    is_answer: choice_bool,
-    question_id: question_id
-  )
-end
 
-
-CSV.foreach('db/csv/words.csv') do |row|
-  level_id = row[CSVROW_LEVELID]
-  subject = row[CSVROW_SUBJECT]
-  meaning_ja = row[CSVROW_MEANING_JA]
-  meaning_en = row[CSVROW_MEANING_EN]
-
-  Word.create!(
-    subject: subject,
-    meaning_ja: meaning_ja,
-    meaning_en: meaning_en,
-    level_id: level_id,
-    image: open("./db/fixtures/rails.png")
-  )
-end
+# CSV.foreach('db/csv/words.csv') do |row|
+#   level_id = row[CSVROW_LEVELID]
+#   subject = row[CSVROW_SUBJECT]
+#   meaning_ja = row[CSVROW_MEANING_JA]
+#   meaning_en = row[CSVROW_MEANING_EN]
+#
+#   Word.create!(
+#     subject: subject,
+#     meaning_ja: meaning_ja,
+#     meaning_en: meaning_en,
+#     level_id: level_id,
+#     image: open("./db/fixtures/rails.png")
+#   )
+# end
 
 (1..3).map do |i|
   User.create!(username: "ユーザー#{i}",
@@ -69,6 +69,6 @@ end
   )
 end
 
-(1..3).map do |i|
-  Level.find_or_create_by(name: "レベル#{i}")
-end
+# (1..3).map do |i|
+#   Level.find_or_create_by(name: "レベル#{i}")
+# end
